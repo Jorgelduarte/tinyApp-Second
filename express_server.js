29e91bab33f4a0af4c964e8102743967ae57b398
Jorgelduarte <duarte.junior@gmail.com>
2018-02-22 05:24:22 +0000

    Create a Registration and Login Page. Set a user_id cookie as ID.

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+var PORT = process.env.PORT || 5000; // default port 8080
 var cookieParser = require('cookie-parser')
 
 app.set("view engine", "ejs");
@@ -20,6 +20,19 @@ function generateRandomString() {
   return text;
 }
 
+// users on database
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "test"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -30,22 +43,64 @@ app.get("/", (req, res) => {
   res.redirect("urls");
 });
 
+// main page
 app.get("/urls", (req, res) => {
   let templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user: users[req.cookies["user_id"]]
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
   let templateVars = {
-    username: req.cookies["username"]
+    user: users[req.cookies["user_id"]]
   };
   res.render("urls_new", templateVars);
 });
 
-//new URL
+// Go to Register page
+app.get("/register", (req, res) => {
+  res.render("registration");
+});
+
+//New user- Check if the email or password is empty. After, if the email exist in database
+// If one of them is false, status 400. Otherwise, create the user in database, set cookies and redirect.
+app.post("/register", (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  let userID = generateRandomString();
+
+  var emptyFields = false;
+  var emailExists = false;
+  for (key in users) {
+    let existingEmail = users[key].email
+    if ((!email) || (!password)) {
+      emptyFields = true;
+    } else if (email === existingEmail) {
+      emailExists = true;
+    } 
+  }
+  
+  if(emptyFields){
+    res.status(400);
+    res.send('Password or email empty');
+  } else if (emailExists){
+    res.status(400);
+    res.send('Email already exist');
+  } else {
+    users[userID] = {
+      id: userID,
+      email: email,
+      password: password
+    };
+    res.cookie("user_id", userID);
+    res.redirect("/urls");
+  }
+});
+
+
+//New URL
 app.post("/urls", (req, res) => {
   let newUrl = req.body.longURL;
   let newShortUrl = generateRandomString()
@@ -58,6 +113,11 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+//Login page
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
 // Edit URls
 app.post("/urls/:id", (req, res) => {
   let updateUrl = req.body.longURL;
@@ -66,16 +126,41 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
-// Set Cookies
+// Login Page - Check if the email exist in database and the password match.
+// If one of them is false, status 403. Otherwise, set cookies and redirect.
 app.post("/login", (req, res) => {
-  let username = req.body.username
-  res.cookie("username", username ) 
-  res.redirect("/urls");
+  let user = "";
+  let passwordMatch = false;
+  let emailExists = false;
+  for (key in users) {
+    let existingEmail = users[key].email
+    let loginEmail = req.body.email
+    let loginPassword = req.body.password
+    
+    if (loginEmail === existingEmail) {
+      emailExists = true;
+    } 
+    if (loginPassword === users[key].password) {
+      passwordMatch = true
+      user = users[key].id
+    }
+  }
+
+  if(!emailExists){
+    res.status(403);
+    res.send('Email cannot be found');
+  } else if (!passwordMatch){
+    res.status(403);
+    res.send('password invalid');
+  } else {
+    res.cookie("user_id", user) 
+    res.redirect("/urls");
+  }
 });
 
 //Logout
 app.post("/logout", (req, res) => {
-  res.clearCookie("username")
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
 
@@ -83,7 +168,7 @@ app.get("/urls/:id", (req, res) => {
   let templateVars = {
     shortURL: req.params.id,
     longURL: urlDatabase[req.params.id],
-    username: req.cookies["username"]
+    user: users[req.cookies["user_id"]]
   };
   res.render("urls_show", templateVars);
 });
